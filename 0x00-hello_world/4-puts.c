5e4bff16cc09f983226c851c8a1c8c5fdb5a7878
mohamed <mohamed.ouz2016@gmail.com>
2023-03-16 15:43:26 -0700

    first code on c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/* the main prints a sentence using puts and return 0 */
+/**
+ * main -  the main prints a sentence using puts
+ * Return:  return 0 
+ */
 int main(void)
 {
 	char sentence[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(sentence);
-	return 0;	
+	return (0);	
 }

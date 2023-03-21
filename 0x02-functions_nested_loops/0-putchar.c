558bf6f6d937ada5fd8b56c0cdcc8b74bf551b7f
mohamed <mohamed.ouz2016@gmail.com>
2023-03-21 08:14:51 -0700

    corection with for loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-
-/*
- * main: prints the keyword _putchar
+#include "main.h"
+/**
+ * main - prints the keyword _putchar
  * Return: return the value 0
  */
 
 int main(void)
 {
-	printf("_putchar");
+	char keyword[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(keyword[i]);
+	}
+	_putchar('\n');
 	return (0);
 }

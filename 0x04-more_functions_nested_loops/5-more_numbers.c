fc1f9e2db09a220f98fa1905c42b8db043bc514d
mohamed <mohamed.ouz2016@gmail.com>
2023-03-23 10:16:13 -0700

    numbers to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14 multipltimes
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int n;
+
+	while (i <=9)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			if ( n > 9)
+				_putchar((n /10) + '0');
+			_putchar((n % 10) + '0');
+			n++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

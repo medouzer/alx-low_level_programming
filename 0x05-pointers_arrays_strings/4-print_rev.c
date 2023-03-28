980b4c089980599b2ac3ed84eae37c22ba3066dc
mohamed <mohamed.ouz2016@gmail.com>
2023-03-27 19:52:31 -0700

    finally

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,13 @@
 
 void print_rev(char *s)
 {
-	int i;
 	int len = 0;
+	int i;
 
-	while (s[i++])
+	while (s[len] != '\0')
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = len -1; i >= 0; i--)
 		_putchar(s[i]);
-
 	_putchar('\n');
 }

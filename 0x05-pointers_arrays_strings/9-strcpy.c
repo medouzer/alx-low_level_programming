41c551460b48e97247276fde653ea92dcad8870d
mohamed <mohamed.ouz2016@gmail.com>
2023-03-29 08:14:17 -0700

    array to array

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,3 +4,20 @@
  * *_strcpy - copy the array into onther one
  * @dest: an array
  * @src: an array
+ * Return: return the last array
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}

7e7b640062fc7c9eed80396575f1463b8fbc7379
mohamed <mohamed.ouz2016@gmail.com>
2023-03-30 08:57:17 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - concatenates two string
+ * @dest: string 1
+ * @src: string 2
+ * @n: number bytes src
+ * Return: return the string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}

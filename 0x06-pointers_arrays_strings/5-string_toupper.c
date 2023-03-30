e912d2db6855c27a6ffd61a41c195b672cf3589f
mohamed <mohamed.ouz2016@gmail.com>
2023-03-30 10:02:00 -0700

    lowwer to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two string
+ * @dest: string 1
+ * @src: string 2
+ * Return: return the string dest
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}

fa8ee73998950b981d53b364a24300169b563fe0
mohamed <mohamed.ouz2016@gmail.com>
2023-04-05 19:12:44 -0700

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - length of a string
  * @s: is the string
+ * Return: return the length
  */
 
 int _strlen_recursion(char *s)
@@ -10,7 +11,9 @@ int _strlen_recursion(char *s)
 	int i = 1;
 
 	if (*s == '\0')
+	{
 		return (0);
+	}
 	else
 		i = i + _strlen_recursion(s + 1);
 

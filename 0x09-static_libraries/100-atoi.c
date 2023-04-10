789d097bdaf17e731dfda752c52367d895f461e8
mohamed <mohamed.ouz2016@gmail.com>
2023-04-10 03:54:06 -0700

    creation of static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,4 +8,18 @@
 
 int _atoi(char *s)
 {
+	int i = 0;
+	unsigned int result = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '_')
+			i = i * 1;
+		else if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
+		*s++;
+	}
+	result = result * i;
+
+	return (result);
 }

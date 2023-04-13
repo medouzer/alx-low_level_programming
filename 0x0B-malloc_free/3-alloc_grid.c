39d5244e4caf3485bcd9230c12d5c048cc004cb7
mohamed <mohamed.ouz2016@gmail.com>
2023-04-12 18:56:22 -0700

    2 dimensional array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return pointer of 2 dimensional
+ * @width: the width of the array
+ * @height: the height of the array
+ * Return: return the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arraygr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arraygr = malloc(height * sizeof(int *));
+
+	if (arraygr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arraygr[i] = malloc(width * sizeof(int));
+
+		if (arraygr[i] == NULL)
+		{
+			free(arraygr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			arraygr[i][j] = 0;
+		}
+	}
+
+	return (arraygr);
+}

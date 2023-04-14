5c931cc01874df49d288e5c1f80dc6a7e9ef3b11
mohamed <mohamed.ouz2016@gmail.com>
2023-04-13 17:56:10 -0700

    fix error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l1 = 0, l2 = 0;
 	unsigned int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[l1] != '\0')
 		l1++;
 

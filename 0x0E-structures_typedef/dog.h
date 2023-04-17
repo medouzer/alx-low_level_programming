78e37c617f37b7b1241399be16c6016943d43961
mohamed <mohamed.ouz2016@gmail.com>
2023-04-16 23:43:59 -0700

    create a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * dog - is a struct of dog
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

9a0544dbb80ee58fba1a11af292520dd6612fb94
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-01 16:31:51 -0700

    fix betty and error of the output

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,15 +16,19 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	tmp = malloc (sizeof(listint_t));
+	tmp = malloc(sizeof(listint_t));
+	if (tmp == NULL)
+		return (NULL);
 
 	tmp->n = n;
 	tmp->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = tmp;
 		return (tmp);
 	}
+
 	ptr = *head;
 	while (ptr->next != NULL)
 		ptr = ptr->next;

a850546e22c50b2f70c07ebed28bca360d56b074
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-02 16:47:26 -0700

    error of the check 9

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,13 +11,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp = *head;
 
-	if (head)
+	while (tmp != NULL)
 	{
-		while (tmp != NULL)
-		{
-			tmp = tmp->next;
-			free(*head);
-			*head = tmp;
-		}
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
 	}
 }

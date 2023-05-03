39d1e994f2aac63a873d21ad7d59c4f629783e75
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-02 19:53:03 -0700

    check 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx > 0)
 	{
-		while (ptr != NULL)
+		while (ptr != NULL && i < idx + 1)
 		{
 			if (i == idx)
 			{

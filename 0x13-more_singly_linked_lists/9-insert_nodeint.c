b9219bc9c08aa649aac56709dd8d4c4245413d2e
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-02 19:46:08 -0700

    check 9 and 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (ptr && i < idx)
 		{
-			if (i == idx - 1)
+			if (i == idx)
 			{
 				tmp->next = ptr->next;
 				ptr->next = tmp;

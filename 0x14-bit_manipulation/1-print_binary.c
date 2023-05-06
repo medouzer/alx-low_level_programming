9d7f4266678a8370352820a9be286ce7e11f1114
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-05 18:06:23 -0700

    fix errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,10 +8,12 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int cur;
 	if (n == 0 || n == 1)
 		putchar(n);
-	if (n > 1)
-		print_binary(n >> 1);
+	cur = n;
+	if (cur > 1)
+		print_binary(cur >> 1);
 
-	putchar((n & 1) + '0');
+	putchar((cur & 1) + '0');
 }

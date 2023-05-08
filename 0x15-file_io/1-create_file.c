936bff2e0b62c3f2dbe2aab20d27a5559e4c9f76
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-08 16:23:23 -0700

    fix errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	while (text_content[i] != '\0')
+	while (text_content != NULL && text_content[i] != '\0')
 		i++;
 	write(file, text_content, i);
 	close(file);

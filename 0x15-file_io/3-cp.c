f5d17841d6ceb5ae07fe57eda2c5473db9279b4d
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-09 19:31:50 -0700

    fix errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	cp_error(file_from, 0, argv);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
 	cp_error(0, file_to, argv);
 	r = 1024;
 	while (r == 1024)

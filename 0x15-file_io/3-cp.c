5c5619797161dff75f38e491d29aa91c2c203a13
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-05-12 16:46:30 -0700

    fix errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 /**
  * cp_error - function return the error about the file
@@ -43,7 +46,7 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	cp_error(file_from, 0, argv);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0674);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	cp_error(0, file_to, argv);
 	r = 1024;
 	while (r == 1024)

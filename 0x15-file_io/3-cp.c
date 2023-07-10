e8751f090f9dd0352a5595e75cc7f9b33623bf82
mohamed <mohamed.ouz2016@gmail.com>
2023-07-10 16:41:26 -0700

    fix errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ void cp_error(int file_from, int file_to, char *argv[])
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }

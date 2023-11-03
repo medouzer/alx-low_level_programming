78bfd5d1250a2312ee8cd6f2e9780baa27eef69c
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-03 16:02:11 -0700

    check 2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,4 +1,4 @@
-#ifndef HASH_TABLES_H
+#ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
 #include <stdlib.h>
@@ -25,13 +25,11 @@ typedef struct hash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
 	unsigned long int size;
-     hash_node_t **array;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);

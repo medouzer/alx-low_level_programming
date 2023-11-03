38a62ec45a069b4725dfb9f3986096db4fa4b7ec
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-03 16:40:42 -0700

    check 2 betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,12 +5,14 @@
  *
  * @key: this is the key to be hashed
  * @size: this is the size of the array of the hash table
- * Return: return the index at which the key/value pair should be stored in the array
+ * Return: return the index at which the key/value pair
+ * should be stored in the array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
+
 	index = hash_djb2(key) % size;
 
 	return (index);

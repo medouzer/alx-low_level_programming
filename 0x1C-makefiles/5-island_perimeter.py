753d43232a61e43faaba985d8412f1f015b26e22
ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-20 20:03:32 +0100

    handle cases

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,12 +7,24 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
-                if (grid[i - 1][j] == 0):
+                if (i > 0):
+                    if (grid[i - 1][j] == 0):
+                        perimeter += 1
+                else:
                     perimeter += 1
-                if (grid[i][j - 1] == 0):
+                if (j > 0):
+                    if (grid[i][j - 1] == 0):
+                        perimeter += 1
+                else:
                     perimeter += 1
-                if (grid[i + 1][j] == 0):
+                if (i < len(grid)):
+                    if (grid[i + 1][j] == 0):
+                        perimeter += 1
+                else:
                     perimeter += 1
-                if (grid[i][j + 1] == 0):
+                if (j < len(grid[i])):
+                    if (grid[i][j + 1] == 0):
+                        perimeter += 1
+                else:
                     perimeter += 1
     return perimeter
